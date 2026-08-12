a0feb3b4e13daaabe620f32d25382f57c79ac6cd
Sagir Ahmed <real.sagirahmed@gmail.com>
2026-08-12 23:41:03 +0600

    Remove Hasher folder to strictly align with requirements

--- Hasher/Program.cs
+++
@@ -1,8 +0,0 @@
-using System;
-class Program {
-    static void Main() {
-        Console.WriteLine("Admin: " + BCrypt.Net.BCrypt.HashPassword("Admin@123"));
-        Console.WriteLine("Teacher: " + BCrypt.Net.BCrypt.HashPassword("Teacher@123"));
-        Console.WriteLine("Student: " + BCrypt.Net.BCrypt.HashPassword("Student@123"));
-    }
-}
